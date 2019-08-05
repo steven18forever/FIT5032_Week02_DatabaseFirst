566a336e5f134b106f0c3005f2914d782d1dce6c
zhiyan <zliu0039@student.monash.edu>
2019-08-05 23:39:55 +1000

    Add project files.

---
+++ FIT5032_Week02_DatabaseFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_Week02_DatabaseFirst.Startup))]
+namespace FIT5032_Week02_DatabaseFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
